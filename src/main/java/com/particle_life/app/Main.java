6b39c0965d6c4b484b4aa494906d7b56d9129373
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2022-02-13 16:58:25 +0100

    display shortcuts in help menu

--- src/main/java/com/particle_life/app/Main.java
+++ src/main/java/com/particle_life/app/Main.java
@@ -688,7 +688,33 @@ public class Main extends App {
 
         if (showHelpWindow.get()) {
             if (ImGui.begin("Help", showHelpWindow)) {
-                ImGui.text("Help text...");
+                ImGui.text("""
+                        [l]/[L]: change palette
+                        [s]/[S]: change shader
+                        [v]/[V]: change accelerator
+                                                
+                        [x]/[X]: change position setter
+                        [r]/[R]: change matrix generator
+                                                
+                        [p]: set positions
+                        [t]: set types
+                        [m]: set matrix
+                                                
+                        [w]: toggle space wrapping
+                                                
+                        [SPACE]: pause physics
+                                                
+                        [F11]: toggle full screen
+                        [ALT]+[F4]: quit
+                                                
+                        [+]/[-]: zoom
+                        [z]: reset zoom
+                        [Z]: reset zoom (fit window)
+                                                
+                        [a]: toggle advanced GUI
+                        [c]: toggle traces (clear screen)
+                        [h]: hide GUI / show GUI
+                        """);
             }
             ImGui.end();
         }
