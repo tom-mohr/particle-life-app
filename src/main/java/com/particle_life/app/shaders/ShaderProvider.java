3f16930de8886c80648f1c32d80acf5a664226c7
tom-mohr <tom-mohr@users.noreply.github.com>
2023-01-20 21:20:29 +0100

    use "repeat" shader by default

--- src/main/java/com/particle_life/app/shaders/ShaderProvider.java
+++ src/main/java/com/particle_life/app/shaders/ShaderProvider.java
@@ -11,10 +11,10 @@ public class ShaderProvider implements InfoWrapperProvider<ParticleShader> {
     public List<InfoWrapper<ParticleShader>> create() {
         List<InfoWrapper<ParticleShader>> shaders = new ArrayList<>();
 
-        shaders.add(new InfoWrapper<>("default",
-                new ParticleShader("shaders/default.vert", "shaders/default.geom", "shaders/default.frag")));
         shaders.add(new InfoWrapper<>("repeat", "Draws copies of particles (without colors) beyond the borders. This is helpful if \"wrap\" is enabled.",
                 new ParticleShader("shaders/default.vert", "shaders/repeat.geom", "shaders/default.frag")));
+        shaders.add(new InfoWrapper<>("default",
+                new ParticleShader("shaders/default.vert", "shaders/default.geom", "shaders/default.frag")));
         shaders.add(new InfoWrapper<>("white", "All particles are white.",
                 new ParticleShader("shaders/white.vert", "shaders/default.geom", "shaders/default.frag")));
         shaders.add(new InfoWrapper<>("velocity brightness", "Brightness based on velocity.",
